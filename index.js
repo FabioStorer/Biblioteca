e6d0be876b89a8c051956658bae33f1c11d1f7c2
Fabio.Storer <fabiostorer19@gmail.com>
2024-06-13 16:51:19 -0300

    feat: codigo atualizado e funcionando

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const biblioteca = {};
+const biblioteca = ['livro1', 'livro2', 'livro3'];
 let livros = {
     nome: '',
     tamanho: '',
@@ -7,6 +7,7 @@ let livros = {
 };
 let opcao = 0;
 let entradaUsuario = '';
+let remover;
 
 console.log('1. Para adicionar um livro novo a biblioteca.');
 console.log('2. Para remover um livro da biblioteca.');
@@ -15,13 +16,25 @@ console.log('3. Para listar os livros da biblioteca.');
 process.stdin.on('data', function (data) {
     entradaUsuario = data.toString().trim();
 
+    if (entradaUsuario == 'Sair' || entradaUsuario == 'sair') {
+        process.exit();
+    }
+    
     if (!opcao) {
         opcao = Number(entradaUsuario);
-    }
+        if (opcao == 1) {
+            console.log('Insira o nome do livro.');
+        } else if (opcao == 2) {
+            console.log('Aqui estão todos os livros na biblioteca atualmente.');
+            for (let i = 0; i < biblioteca.length; i++) {
+                console.log([i], ' - ', biblioteca[i]);
+            }
+            console.log('Escolha pelo índice qual livro deseja remover.');
+        }
+    } else
     
     switch (opcao) {
         case 1:
-            console.log('Insira o nome do livro.');
             if (!livros.nome) {
                 livros.nome = entradaUsuario;
                 console.log('Insira a quantidade de páginas que o livro possui.');
@@ -34,20 +47,52 @@ process.stdin.on('data', function (data) {
             } else {
                 livros.genero = entradaUsuario;
                 biblioteca.push(livros);
+                livros = {
+                    nome: '',
+                    tamanho: '',
+                    autor: '',
+                    genero: ''
+                };
                 console.log(biblioteca);
+                console.log('Escolha uma nova opção ou digite "Sair" para finalizar.');
+                console.log('1. Para adicionar um livro novo a biblioteca.');
+                console.log('2. Para remover um livro da biblioteca.');
+                console.log('3. Para listar os livros da biblioteca.');
+                opcao = 0;
             }
-
+            
             break;
+            
+            case 2:
+                if (biblioteca == false) {
+                    console.log('Biblioteca vazia.');
+                } else {
+                    remover = Number(entradaUsuario);
+                    biblioteca.splice(remover, 1);
+                    console.log(remover)
+                }
+                console.log(biblioteca);
+                console.log('Escolha uma nova opção ou digite "Sair" para finalizar.');
+                console.log('1. Para adicionar um livro novo a biblioteca.');
+                console.log('2. Para remover um livro da biblioteca.');
+                console.log('3. Para listar os livros da biblioteca.');
+                opcao = 0;
+                
+                break;
+                
+                case 3:
+                    console.log(biblioteca);
+                    console.log('Escolha uma nova opção ou digite "Sair" para finalizar.');
+                    console.log('1. Para adicionar um livro novo a biblioteca.');
+                    console.log('2. Para remover um livro da biblioteca.');
+                console.log('3. Para listar os livros da biblioteca.');
+                opcao = 0;
 
-        case 2:
+                break;
 
-            break;
-
-        case 3:
-
-            break;
-
-        default:
-            break;
-    }
+            default:
+                console.log('Insira uma opção válida.');
+                opcao = 0;
+                break;
+        }
 })
